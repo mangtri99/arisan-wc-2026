12586e0e999bce3e171f192b9b4c1c9907db51da
Mang Tri <mangtri93@gmail.com>
2026-06-10 10:52:24 +0800

    feat: tambah Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import './style.css'
 
+inject()
 createApp(App).use(createPinia()).mount('#app')
