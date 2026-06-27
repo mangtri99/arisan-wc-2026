f638960facdc12c1dfedb6fffde072e4931f6e57
Mang Tri <mangtri93@gmail.com>
2026-06-27 14:11:49 +0800

    test: change test environment to jsdom for localStorage support

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   test: {
-    environment: 'node',
+    environment: 'jsdom',
     include: ['src/**/*.test.ts'],
   },
 })
